2ee53f47180758a7930f74da13105f1645a72ba9
nickangtc <nickangtc@gmail.com>
2016-09-13 15:06:30 +0800

    Made endpoints for get and post

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// configure app to use ejs as view engine
+app.set('view engine', 'ejs');
+
+// configure body-parser
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+var SCORES = [
+  {
+    id: 1,
+    name: 'Eugene Ooi',
+    score: 99
+  },
+  {
+    id: 2,
+    name: 'Nick Ang',
+    score: 99
+  }
+];
+
+// CREATE new entry
+app.get('/entries/new', function (req, res) {
+  res.render('entry_new');
+});
+
+app.post('/entries', function (req, res) {
+  var newId = SCORES.length + 1;
+  var newEntry = {
+    id: newId,
+    name: req.body.name,
+    score: req.body.score
+  };
+  SCORES.push(newEntry);
+
+  res.redirect('/entries/' + newId);
+});
+
+// READ scores
+app.get('/entries', function (req, res) {
+  res.json(SCORES);
+});
+
+// READ score of one entry
+app.get('/entries/:id', function (req, res) {
+  var id = parseInt(req.params.id);
+  var result = {};
+
+  for (var i = 0; i < SCORES.length; i++) {
+    if (SCORES[i].id === id) {
+      result = SCORES[i];
+    }
+  }
+  res.json(result);
+});
+
+console.log('You\'re surfing the big waves of http://localhost:3000');
+app.listen(3000);
